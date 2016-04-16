c4cdba18475f65f6b7940d38a58ef5bbd0273e7b
Yaroslav Serhieiev <noomorph@gmail.com>
2016-04-16 20:43:20 +0300

    feat: added implementation for Yakhin-Mendelevich

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import factor5 from "./factor5/factor5";
 import mmpi from "./mmpi/mmpi";
 import spa from "./spa/spa";
 import usk from "./usk/usk";
+import mendel from "./mendel/mendel";
 
 export default {
     alexithymia,
     amon,
     emin,
     factor5,
+    mendel,
     mmpi,
     spa,
     usk,
