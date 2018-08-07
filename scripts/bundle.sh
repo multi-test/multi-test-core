713e0eb206bde87a7c1ba1ea1291995f75c4811e
Yaroslav Serhieiev <yaroslavs@wix.com>
2018-08-07 11:06:26 +0300

    versions: bumped package dependencies to the latest ones

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -4,16 +4,16 @@ tsc -m es6
 rm -rf lib
 mkdir lib
 
-(rollup dist/src/index.js --file lib/index.js --format cjs) & \
-(rollup dist/src/alexithymia/alexithymia.js --file lib/alexithymia.js --format cjs) & \
-(rollup dist/src/amon/amon.js --file lib/amon.js --format cjs) & \
-(rollup dist/src/cattell/cattell.js --file lib/cattell.js --format cjs) & \
-(rollup dist/src/emin/emin.js --file lib/emin.js --format cjs) & \
-(rollup dist/src/factor5/factor5.js --file lib/factor5.js --format cjs) & \
-(rollup dist/src/lsi/lsi.js --file lib/lsi.js --format cjs) & \
-(rollup dist/src/maddi/maddi.js --file lib/maddi.js --format cjs) & \
-(rollup dist/src/mendel/mendel.js --file lib/mendel.js --format cjs) & \
-(rollup dist/src/mmpi/mmpi.js --file lib/mmpi.js --format cjs) & \
-(rollup dist/src/spa/spa.js --file lib/spa.js --format cjs) & \
-(rollup dist/src/usk/usk.js --file lib/usk.js --format cjs) & \
+(rollup dist/src/index.js --file lib/index.js -c scripts/rollup.config.js) & \
+(rollup dist/src/alexithymia/alexithymia.js --file lib/alexithymia.js -c scripts/rollup.config.js) & \
+(rollup dist/src/amon/amon.js --file lib/amon.js -c scripts/rollup.config.js) & \
+(rollup dist/src/cattell/cattell.js --file lib/cattell.js -c scripts/rollup.config.js) & \
+(rollup dist/src/emin/emin.js --file lib/emin.js -c scripts/rollup.config.js) & \
+(rollup dist/src/factor5/factor5.js --file lib/factor5.js -c scripts/rollup.config.js) & \
+(rollup dist/src/lsi/lsi.js --file lib/lsi.js -c scripts/rollup.config.js) & \
+(rollup dist/src/maddi/maddi.js --file lib/maddi.js -c scripts/rollup.config.js) & \
+(rollup dist/src/mendel/mendel.js --file lib/mendel.js -c scripts/rollup.config.js) & \
+(rollup dist/src/mmpi/mmpi.js --file lib/mmpi.js -c scripts/rollup.config.js) & \
+(rollup dist/src/spa/spa.js --file lib/spa.js -c scripts/rollup.config.js) & \
+(rollup dist/src/usk/usk.js --file lib/usk.js -c scripts/rollup.config.js) & \
 wait
---
+++ scripts/rollup.config.js
@@ -0,0 +1,6 @@
+export default {
+  external: ['tslib'],
+  output: {
+    format: 'cjs',
+  },
+};
